dd44ae3e5d0750c0d2e6359ce3749961a9698eb3
JunHyeong Lim <entrypoint_@naver.com>
2018-11-03 21:43:00 +0900

    Configuration base

---
+++ src/test/java/com/github/entrypointkr/junlib/ConfigurationTestObject.java
@@ -0,0 +1,40 @@
+package com.github.entrypointkr.junlib;
+
+import com.github.entrypointkr.junlib.bukkit.configuration.ReflectiveYaml;
+import com.google.common.collect.ImmutableMap;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Map;
+
+/**
+ * Created by JunHyeong on 2018-11-03
+ */
+public class ConfigurationTestObject {
+    private final ReflectiveYaml config = new ReflectiveYaml();
+    private final Map<String, TestObject> testMap = ImmutableMap.<String, TestObject>builder()
+            .put("a", new TestObject("a1", "a2"))
+            .put("b", new TestObject("b1", "b2"))
+            .build();
+
+    @Test
+    public void test() {
+        StringWriter writer = new StringWriter();
+        config.write(testMap, writer);
+        Map<String, TestObject> newMap = config.<Map<String, TestObject>>read(new StringReader(writer.toString())).orElseThrow(() -> new AssertionError("a"));
+        Assert.assertEquals(testMap, newMap);
+    }
+
+    static class TestObject {
+        private final String a;
+        private final String b;
+        private transient int c;
+
+        public TestObject(String a, String b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+}
