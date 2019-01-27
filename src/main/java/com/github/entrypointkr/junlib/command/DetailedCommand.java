d7815d1eabde928f1e192efad215d2f1697cabd4
EntryPointKR <entrypoint_@naver.com>
2019-01-27 15:46:50 +0900

    Remove old command api

--- src/main/java/com/github/entrypointkr/junlib/command/DetailedCommand.java
+++
@@ -1,9 +0,0 @@
-package com.github.entrypointkr.junlib.command;
-
-import com.github.entrypointkr.junlib.util.ArrayReader;
-
-/**
- * Created by JunHyeong on 2018-10-26
- */
-public interface DetailedCommand<T, U extends ArrayReader<String>> extends TabCompleteCommand<T, U>, CommandInformation {
-}
--- src/main/java/com/github/entrypointkr/junlib/command/TabCompleteCommand.java
+++
@@ -1,9 +0,0 @@
-package com.github.entrypointkr.junlib.command;
-
-import com.github.entrypointkr.junlib.util.ArrayReader;
-
-/**
- * Created by JunHyeong on 2018-10-27
- */
-public interface TabCompleteCommand<T, U extends ArrayReader<String>> extends ICommand<T, U>, TabCompler<T, U> {
-}
